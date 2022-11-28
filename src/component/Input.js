171b8dc30dde74d1738ffaff072265f363ba13e6
김승헌 <tmdgjs818@gmail.com>
2022-11-28 11:22:04 +0900

    Initial commit

---
+++ src/component/Input.js
@@ -0,0 +1,7 @@
+function Input() {
+  //input을 컴포넌트로 만들어봅니다.
+  //input도 컴포넌트도 만들게 되면 로직을 조금 더 고민을 해야 합니다.
+  //고민을 한 번 해보고, 시도 해보세요!
+}
+
+export default Input;
---
+++ src/util/useInput.js
@@ -0,0 +1,8 @@
+//이 곳에 input custom hook을 만들어 보세요.
+//return 해야 하는 값은 배열 형태의 값이어야 합니다.
+
+function useInput() {
+  return [];
+}
+
+export default useInput;
