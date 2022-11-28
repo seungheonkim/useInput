4783535511520a88d5259ad749eb38dd0f0114ca
seungheonkim <seungheon818@naver.com>
2022-11-28 18:11:55 +0900

    useInput-reference

--- src/App.js
+++ src/App.js
@@ -1,53 +1,39 @@
-import { useState } from "react";
+import {useState} from "react";
 import useInput from "./util/useInput";
 import Input from "./component/Input";
 import "./styles.css";
 
 export default function App() {
-  //input에 들어가는 상태값 및 로직을 custom hook으로 만들어봅니다.
-  //until 폴더의 useInput.js 파일이 만들어져 있습니다.
-  const [firstNameValue, setFirstNameValue] = useState("");
-  const [lastNameValue, setLastNameValue] = useState("");
-  const [nameArr, setNameArr] = useState([]);
+    //input에 들어가는 상태값 및 로직을 custom hook으로 만들어봅니다.
+    //until 폴더의 useInput.js 파일이 만들어져 있습니다.
+    const [firstNameValue, firstNameBind, firstReset] = useInput("");
+    const [lastNameValue, lastNameBind, lastReset] = useInput("");
+    const [nameArr, setNameArr] = useState([]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setNameArr([...nameArr, `${firstNameValue} ${lastNameValue}`]);
-    setFirstNameValue('');
-    setLastNameValue('');
-  };
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setNameArr([...nameArr, `${firstNameValue} ${lastNameValue}`]);
+        firstReset('');
+        lastReset('');
+    };
 
-  return (
-    <div className="App">
-      <h1>Name List</h1>
-      <div className="name-form">
-        <form onSubmit={handleSubmit}>
-          <div className="name-input">
-            <label>성</label>
-            <input
-              value={firstNameValue}
-              onChange={(e) => setFirstNameValue(e.target.value)}
-              type="text"
-            />
-          </div>
-          <div className="name-input">
-            <label>이름</label>
-            <input
-              value={lastNameValue}
-              onChange={(e) => setLastNameValue(e.target.value)}
-              type="text"
-            />
-          </div>
-          <button>제출</button>
-        </form>
-      </div>
-      <div className="name-list-wrap">
-        <div className="name-list">
-          {nameArr.map((el, idx) => {
-            return <p key={idx}>{el}</p>;
-          })}
+    return (
+        <div className="App">
+            <h1>Name List</h1>
+            <div className="name-form">
+                <form onSubmit={handleSubmit}>
+                    <Input value={firstNameBind} labelText={'성'}/>
+                    <Input value={lastNameBind} labelText={'이름'}/>
+                    <button>제출</button>
+                </form>
+            </div>
+            <div className="name-list-wrap">
+                <div className="name-list">
+                    {nameArr.map((el, idx) => {
+                        return <p key={idx}>{el}</p>;
+                    })}
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
